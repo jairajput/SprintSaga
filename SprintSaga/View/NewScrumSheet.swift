35c418c16e35cc2f5346d9622ec2d429be20c1a9
Jai <jai07rajput@gmail.com>
2023-11-30 09:50:02 +0530

    ScrumSheet

---
+++ SprintSaga/View/NewScrumSheet.swift
@@ -0,0 +1,39 @@
+//
+//  NewScrumSheet.swift
+//  SprintSaga
+//
+//  Created by Jai  on 30/11/23.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    @State private var newScrum = DailyScrum.emptyScrum
+    @Binding var scrums: [DailyScrum]
+    @Binding var isPresentingNewScrumView: Bool
+    
+    var body: some View {
+        NavigationStack {
+            DetailEditView(scrum: $newScrum)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Dismiss") {
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Add") {
+                            scrums.append(newScrum)
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+struct NewScrumSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        NewScrumSheet(scrums: .constant(DailyScrum.sampleData), isPresentingNewScrumView: .constant(true))
+    }
+}
--- SprintSaga/View/ScrumsView.swift
+++ SprintSaga/View/ScrumsView.swift
@@ -7,30 +7,41 @@
 
 import SwiftUI
 
-
 struct ScrumsView: View {
-    let scrums: [DailyScrum]
-  
+    @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var isPresentingNewScrumView = false
+    let saveAction: ()->Void
+
     var body: some View {
         NavigationStack {
-            List(scrums) { scrum in
-                NavigationLink(destination:DetailView(scrum: scrum)) {
+            List($scrums) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
                     CardView(scrum: scrum)
-                        .listRowBackground(scrum.theme.mainColor)
                 }
                 .listRowBackground(scrum.theme.mainColor)
             }
-            .navigationTitle("Daily Scrum")
-            .toolbar{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/)  {
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
                     Image(systemName: "plus")
                 }
                 .accessibilityLabel("New Scrum")
             }
         }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
     }
 }
 
-#Preview {
-    ScrumsView(scrums: DailyScrum.sampleData)
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
+    }
 }
