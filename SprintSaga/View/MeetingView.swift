b2847af49affd68dc248bfbc1ec3fae5cd4f2795
Jai <jai07rajput@gmail.com>
2023-11-30 09:52:47 +0530

    Meetingheader

--- SprintSaga/View/MeetingView.swift
+++ SprintSaga/View/MeetingView.swift
@@ -6,54 +6,61 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
     var body: some View {
-        VStack {
-            ProgressView(value:5,total: 15)
-            HStack{
-                VStack(alignment: .leading){
-                    Text("Time Elapsed")
-                        .font(.caption)
-                        
-                        .background(Color.red)
-                        .cornerRadius(5)
-                       Label("300", systemImage: "timer")
-                        .foregroundColor(.red)
-                }
-                Spacer()
-                VStack(alignment: .trailing){
-                    Text("Second Remaning")
-                        .font(.caption)
-                        .background(Color.gray)
-                        .cornerRadius(5)
-                    Label("600", systemImage: "timer.circle")
-                        .foregroundColor(.gray)
-                }
-                
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time Remaning")
-            .accessibilityValue("10 Minutes ")
-                Circle()
-                    .strokeBorder(lineWidth: 24)
-                    .foregroundColor(.blue)
-            HStack{
-                Text("Speaker 1 Of 3")
-                    .foregroundStyle(.gray)
-                    .font(.title3)
-                Spacer()
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                 Image(systemName: "goforward.plus")
-                        .font(.system(size: 20))
-                }
-                .accessibilityLabel("Next speaker")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, isRecording: isRecording, theme: scrum.theme)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            startScrum()
+        }
+        .onDisappear {
+            endScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private func startScrum() {
+        scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+        scrumTimer.speakerChangedAction = {
+            player.seek(to: .zero)
+            player.play()
+        }
+        speechRecognizer.resetTranscript()
+        speechRecognizer.startTranscribing()
+        isRecording = true
+        scrumTimer.startScrum()
+    }
+    
+    private func endScrum() {
+        scrumTimer.stopScrum()
+        speechRecognizer.stopTranscribing()
+        isRecording = false
+        let newHistory = History(attendees: scrum.attendees,
+                                 transcript: speechRecognizer.transcript)
+        scrum.history.insert(newHistory, at: 0)
     }
 }
 
-#Preview {
-    MeetingView()
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
 }
