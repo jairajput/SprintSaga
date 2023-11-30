e6104a220bab3f85463209679b66fe45ffcee5f3
Jai <jai07rajput@gmail.com>
2023-11-30 10:01:21 +0530

    historyModel

---
+++ SprintSaga/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  SprintSaga
+//
+//  Created by Jai  on 30/11/23.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var transcript: String?
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], transcript: String? = nil) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.transcript = transcript
+    }
+}
