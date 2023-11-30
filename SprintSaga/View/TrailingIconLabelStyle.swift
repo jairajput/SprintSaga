6d074a3e970c11b583409606c8820ed8f6e0a4c8
Jai <jai07rajput@gmail.com>
2023-11-30 09:45:24 +0530

    TrailingIconlabel

---
+++ SprintSaga/View/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  SprintSaga
+//
+//  Created by Jai  on 30/11/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
