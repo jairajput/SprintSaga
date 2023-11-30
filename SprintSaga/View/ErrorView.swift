0b7210bc0aaee9535d2b966a2f2ec46332b10d61
Jai <jai07rajput@gmail.com>
2023-11-30 09:56:33 +0530

    ErrorView

---
+++ SprintSaga/View/ErrorView.swift
@@ -0,0 +1,54 @@
+//
+//  ErrorView.swift
+//  SprintSaga
+//
+//  Created by Jai  on 30/11/23.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
