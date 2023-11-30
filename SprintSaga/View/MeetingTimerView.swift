cb4439bdd77ae831f27d7f8e5af5e837e382c3b0
Jai <jai07rajput@gmail.com>
2023-11-30 09:51:52 +0530

    Mettingview

---
+++ SprintSaga/View/MeetingTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingTimerView.swift
+//  SprintSaga
+//
+//  Created by Jai  on 30/11/23.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingTimerView()
+}
