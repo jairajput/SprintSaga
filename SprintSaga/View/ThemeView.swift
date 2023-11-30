2188780a079b2dee1cc3a349cbeb6430759c93e5
Jai <jai07rajput@gmail.com>
2023-11-30 09:41:36 +0530

    ThemeView

---
+++ SprintSaga/View/ThemeView.swift
@@ -0,0 +1,27 @@
+//
+//  ThemeView.swift
+//  SprintSaga
+//
+//  Created by Jai  on 30/11/23.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
